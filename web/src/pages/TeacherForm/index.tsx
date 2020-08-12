6b958290dd7d1db8ae59e466695e83f0f64da8c4
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-12 18:50:01 -0300

    Redirect to landing

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
 import Textarea from '../../components/Textarea';
@@ -12,6 +13,8 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm() {
+    const history = useHistory();
+
     const [name, setName] = useState('');
     const [avatar, setAvatar] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
@@ -40,9 +43,10 @@ function TeacherForm() {
             cost: Number(cost),
             schedule: scheduleItems
         }).then(() => {
-            alert('Cadastro realizado com sucesso!')
+            alert('Cadastro realizado com sucesso!');
+            history.push('/')
         }).catch(() => {
-            alert('Erro no cadastro.')
+            alert('Erro no cadastro.');
         });
     }
 
